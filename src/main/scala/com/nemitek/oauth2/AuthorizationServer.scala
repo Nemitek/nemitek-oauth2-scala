c986ccbea51c693c532508944226155f0729d8fe
nemitek <mkotlikov@gmail.com>
2015-07-19 00:46:31 +0000

    add .gitignore file

--- src/main/scala/com/nemitek/oauth2/AuthorizationServer.scala
+++ src/main/scala/com/nemitek/oauth2/AuthorizationServer.scala
@@ -1,5 +1,28 @@
 package com.nemitek.oauth2
 
+import akka.io.IO
+import spray.can.Http
+import spray.http._
+import akka.actor._
+import akka.pattern.ask
+import spray.routing.SimpleRoutingApp
+import scala.xml.NodeSeq
+import scala.concurrent.duration._
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.Await
+
 object AuthorizationServer extends App {
-  println("Hello Nemitek!")
+  // we need an ActorSystem to host our application in??????
+  implicit val system = ActorSystem("nemitek-oauth2-authorization-server")
+
+//  val commodities = system.actorOf(Props[], "commodities")
+
+  // create and start our service actor
+  val service = system.actorOf(Props[AuthServiceActor], "connections")
+
+  implicit val timeout = Timeout(5.seconds)
+  // start a new HTTP server on port 8080 with our service actor as the handler
+  IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 8080)
 }
