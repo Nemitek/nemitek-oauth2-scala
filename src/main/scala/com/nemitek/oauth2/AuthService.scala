411830e2407e041b2836747b846e14ea84fe9e0e
nemitek <mkotlikov@gmail.com>
2015-07-26 22:34:03 +0000

    add token route

--- src/main/scala/com/nemitek/oauth2/AuthService.scala
+++ src/main/scala/com/nemitek/oauth2/AuthService.scala
@@ -1,9 +1,13 @@
 package com.nemitek.oauth2
 
 import akka.actor.Actor
+import spray.httpx.{SprayJsonSupport, Json4sSupport}
+import spray.json.DefaultJsonProtocol
 import spray.routing._
 import spray.http._
 import MediaTypes._
+
+
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -19,6 +23,14 @@ class AuthServiceActor extends Actor with AuthService {
   def receive = runRoute(myRoute)
 }
 
+//grant_type=password&username=USERNAME&password=PASSWORD
+case class PasswordTokenRequest(grant_type: String, username: String, password: String)
+
+case class TokenRequest(grant_type: String, username: String, password: String, client_id: String, client_secret: String, refresh_token: String)
+
+//object PersonJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
+//  implicit val PortofolioFormats = jsonFormat2(PasswordTokenRequest)
+//}
 
 // this trait defines our service behavior independently from the service actor
 trait AuthService extends HttpService {
@@ -26,14 +38,27 @@ trait AuthService extends HttpService {
   val myRoute =
     path("token") {
       post {
-        // token?grant_type=password&username=USERNAME&password=PASSWORD&client_id=CLIENT_ID
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-          complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
-              </body>
-            </html>
+        entity(as[TokenRequest]) {
+          // token?grant_type=password&username=USERNAME&password=PASSWORD&client_id=CLIENT_ID
+          case ("password", _, _, _, _, _) => respondWithMediaType(`text/html`) {
+            // XML is marshalled to `text/xml` by default, so we simply override here
+            complete {
+              <html>
+                <body>
+                  <h1>Say hello to
+                    <i>spray-routing</i>
+                    on
+                    <i>spray-can</i>
+                    !</h1>
+                </body>
+              </html>
+            }
+          }
+          case _ => respondWithMediaType(`text/html`) {
+            // XML is marshalled to `text/xml` by default, so we simply override here
+            complete {
+                    "Unsupported grant type."
+            }
           }
         }
       }
